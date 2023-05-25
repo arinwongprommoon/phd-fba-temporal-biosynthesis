6aa824214cec145bb7cc1e5039769e34b8e18392
Arin Wongprommoon <arin.wongprommoon@ed.ac.uk>
2023-05-25 17:40:49 +0100

    change(dev): model
    WHY IS THIS CHANGE NEEDED?:
    - to see if alternative ablation gives similar conclusions

--- dev_yeast8model.py
+++ dev_yeast8model.py
@@ -3,7 +3,7 @@
 import matplotlib.pyplot as plt
 from yeast8model import Yeast8Model, compare_fluxes, compare_ablation_times
 
-y = Yeast8Model("./models/ecYeastGEM_batch.xml")
+# y = Yeast8Model("./models/ecYeastGEM_batch.xml")
 # y.knock_out_list(["YML120C"])
 # y.knock_out_list(["YML120C", "foo"])
 
@@ -16,15 +16,18 @@ y = Yeast8Model("./models/ecYeastGEM_batch.xml")
 
 # y.optimize()
 
-y.ablation_result = y.ablate()
+# y.ablation_result = y.ablate()
+# fig, ax = plt.subplots()
+# y.ablation_barplot(ax)
+# plt.show()
+
+z = Yeast8Model("./models/ecYeastGEM_batch.xml")
+z.make_auxotroph("BY4741")
+
+z.ablation_result = z.ablate()
 fig, ax = plt.subplots()
-y.ablation_barplot(ax)
+z.ablation_barplot(ax)
 plt.show()
-
-# z = Yeast8Model("./models/ecYeastGEM_batch.xml")
-# z.make_auxotroph("BY4741")
-
-# z.ablation_result = z.ablate()
 
 # fig, ax = plt.subplots()
 # compare_ablation_times(z.ablation_result, y.ablation_result, ax)
