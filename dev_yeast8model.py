fix!(obj): axes rotation in ablation_grid
WHY IS THIS CHANGE NEEDED?:

HOW DOES THE CHANGE SOLVE THE PROBLEM?:
- make array definition intuitive, i.e.

  for x_index, x in enumerate(x_axis):
      for y_index, y in enumerate(y_axis):
          output = do_something(x, y)
          array[x_index, y_index] = output

  this makes further indexing easy & intuitive

- let the plotting function deal with formatting the array for
  presentation.  this separates computation and drawing.
- essentially, this is Tesler's Law, in a way.

dev_yeast8model.py
@@ -27,17 +27,16 @@ y = Yeast8Model("./models/ecYeastGEM_batch_8-6-0.xml")
 # r = y.get_ablation_ratio()
 # print(r)
 exch_rate_dict = {
-    "r_1714": np.linspace(0, 18, 5),
-    "r_1654": np.linspace(0, 18, 5),
+    "r_1714": np.linspace(0, 18, 2),
+    "r_1654": np.linspace(0, 18, 2),
 }
 ra, la = y.ablation_grid(exch_rate_dict)
 breakpoint()
 
-# fig, ax = plt.subplots()
-# heatmap_ablation_grid(ra, exch_rate_dict, ax)
-# fig, ax = plt.subplots()
+fig, ax = plt.subplots()
+heatmap_ablation_grid(ra, exch_rate_dict, ax)
 # y.ablation_barplot(ax)
-# plt.show()
+plt.show()
 
 # z = Yeast8Model("./models/ecYeastGEM_batch.xml")
 # z.make_auxotroph("BY4741")
@@ -50,4 +49,4 @@ breakpoint()
 
 # dfs = compare_fluxes(y, z)
 
-# breakpoint()
+breakpoint()

yeast8model.py
@@ -819,9 +819,7 @@ class Yeast8Model:
                 # DEBUG
                 print(ablation_result)
 
-        return np.flip(ratio_array.T, axis=1), np.flip(
-            largest_component_array.T, axis=1
-        )
+        return ratio_array, largest_component_array
 
 
 def compare_fluxes(ymodel1, ymodel2):
@@ -1019,7 +1017,7 @@ def heatmap_ablation_grid(ratio_array, exch_rate_dict, ax):
 
     """
     sns.heatmap(
-        data=ratio_array,
+        data=np.rot90(ratio_array),
         xticklabels=list(exch_rate_dict.values())[0],
         yticklabels=list(exch_rate_dict.values())[1][::-1],
         cbar_kws={"label": "ratio"},