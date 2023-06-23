195d19f629be300cc3ce633ebce6f9edd15b72a9
arinwongprommoon <arin.wongprommoon@ed.ac.uk>
2023-06-23 11:24:37 +0100

    change: histogram display adjustments

--- dev_abl-vs-enz-use.py
+++ dev_abl-vs-enz-use.py
@@ -3,6 +3,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import seaborn as sns
 
 from yeast8model import Yeast8Model
 
@@ -22,11 +23,19 @@ for biomass_component, fluxes in ablation_fluxes_diff.items():
     ablation_fluxes_diff[biomass_component] = (
         ablation_fluxes[biomass_component] - ablation_fluxes["original"]
     )
+    print(f"{biomass_component}")
+    print(f"min {1e5 * ablation_fluxes_diff[biomass_component].min()} * 1e-5")
+    print(f"max {1e5 * ablation_fluxes_diff[biomass_component].max()} * 1e-5")
 
 breakpoint()
 
 fig, ax = plt.subplots(nrows=len(ablation_fluxes_diff), ncols=1)
 for idx, (biomass_component, fluxes) in enumerate(ablation_fluxes_diff.items()):
-    fluxes.plot.hist(ax=ax[idx])
+    sns.histplot(
+        fluxes, bins=100, binrange=(-16e-5, +90e-5), log_scale=(False, True), ax=ax[idx]
+    )
+    ax[idx].set_title(biomass_component)
 
 breakpoint()
+
+plt.show()
