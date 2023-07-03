82a62eaf8273652363b29494df9ca9dd2eb2ff3d
arinwongprommoon <arin.wongprommoon@ed.ac.uk>
2023-07-03 15:29:11 +0100

    feat(dev): calculate array gradient & plot

--- dev_grid.py
+++ dev_grid.py
@@ -29,8 +29,8 @@ print(largest_component)
 breakpoint()
 
 exch_rate_dict = {
-    "r_1714": np.linspace(0, 2 * 8.45, 3),  # glucose
-    "r_1654": np.linspace(0, 2 * 1.45, 3),  # ammonium
+    "r_1714": np.linspace(0, 2 * 8.45, 5),  # glucose
+    "r_1654": np.linspace(0, 2 * 1.45, 5),  # ammonium
 }
 ablation_result_array = wt_ec.ablation_grid(exch_rate_dict)
 print(ablation_result_array)
@@ -39,6 +39,7 @@ breakpoint()
 
 ratio_array = vget_ablation_ratio(ablation_result_array)
 largest_component_array = vget_ablation_largest_component(ablation_result_array)
+ratio_gradient = np.gradient(ratio_array)
 
 breakpoint()
 
@@ -46,6 +47,21 @@ fig, ax = plt.subplots()
 heatmap_ablation_grid(ax, exch_rate_dict, ratio_array, percent_saturation=True)
 ax.set_xlabel("Glucose exchange (% max = 16.9)")
 ax.set_ylabel("Ammonium exchange (% max = 2.9)")
+ax.set_title("Ratio")
 plt.show()
 
 breakpoint()
+
+fig, ax = plt.subplots()
+heatmap_ablation_grid(ax, exch_rate_dict, ratio_gradient[0], percent_saturation=True)
+ax.set_xlabel("Glucose exchange (% max = 16.9)")
+ax.set_ylabel("Ammonium exchange (% max = 2.9)")
+ax.set_title("Gradient, axis 0")
+plt.show()
+
+fig, ax = plt.subplots()
+heatmap_ablation_grid(ax, exch_rate_dict, ratio_gradient[1], percent_saturation=True)
+ax.set_xlabel("Glucose exchange (% max = 16.9)")
+ax.set_ylabel("Ammonium exchange (% max = 2.9)")
+ax.set_title("Gradient, axis 1")
+plt.show()
