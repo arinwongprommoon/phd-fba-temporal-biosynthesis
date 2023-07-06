584b61c5940e4f8ddd45a63d818c71d5f47c280d
Arin Wongprommoon <arin.wongprommoon@ed.ac.uk>
2023-07-06 18:04:09 +0100

    refactor(scripts/piechart): use dict to specify options
    WHY IS THIS CHANGE NEEDED?:
    - ...rather than commenting out blocks of code

--- scripts/script_piechart.py
+++ scripts/script_piechart.py
@@ -8,34 +8,51 @@ from src.data.biomasscomponent import biomass_component_list_orig
 from src.gem.yeast8model import Yeast8Model
 from src.viz.grid import piechart_ablation_grid
 
-# ec
-# glc_exch_rate = 16.89
-# wt_ec = Yeast8Model("../data/gemfiles/ecYeastGEM_batch_8-6-0.xml")
-# wt_ec.model.reactions.get_by_id("r_1714").bounds = (-glc_exch_rate, 0)
-# wt_ec.model.reactions.get_by_id("r_1714_REV").bounds = (0, glc_exch_rate)
-
-# non-ec
-glucose_bounds = (-4.75, 0)  # gives a sensible growth rate for wt
-wt_y8 = Yeast8Model(
-    "../data/gemfiles/yeast-GEM_8-6-0.xml", growth_id="r_2111", biomass_id="r_4041"
-)
-wt_y8.biomass_component_list = biomass_component_list_orig
-wt_y8.model.reactions.r_1714.bounds = glucose_bounds
-wt_y8.add_media_components(["r_1992"])
-
-# exch_rate_dict = {
-#     "r_1714": np.linspace(0, 2 * 8.45, 8),  # glucose
-#     "r_2033": np.linspace(0, 2 * 4.27, 8), # pyruvate
-#     "r_1654": np.linspace(0, 2 * 1.45, 8),  # ammonium
-# }
-# ablation_result_array = wt_ec.ablation_grid(exch_rate_dict)
-
-exch_rate_dict = {
-    #     "r_1714": np.linspace(0, 2 * 4.75, 8),  # glucose
-    "r_2033": np.linspace(0, 2 * 13.32, 8),  # pyruvate
-    "r_1654": np.linspace(0, 2 * 2.88, 8),  # ammonium
+plot_options = {
+    # "ec" or "y8"
+    "model": "ec",
+    # "glc" or "pyr"
+    "carbon_source": "glc",
 }
-ablation_result_array = wt_y8.ablation_grid(exch_rate_dict)
+
+if plot_options["model"] == "ec":
+    glc_exch_rate = 16.89
+    wt = Yeast8Model("../data/gemfiles/ecYeastGEM_batch_8-6-0.xml")
+    wt.model.reactions.get_by_id("r_1714").bounds = (-glc_exch_rate, 0)
+    wt.model.reactions.get_by_id("r_1714_REV").bounds = (0, glc_exch_rate)
+
+    exch_rate_dict = {
+        "r_1714": np.linspace(0, 2 * 8.45, 8),  # glucose
+        "r_2033": np.linspace(0, 2 * 4.27, 8),  # pyruvate
+        "r_1654": np.linspace(0, 2 * 1.45, 8),  # ammonium
+    }
+
+elif plot_options["model"] == "y8":
+    glucose_bounds = (-4.75, 0)  # gives a sensible growth rate for wt
+    wt = Yeast8Model(
+        "../data/gemfiles/yeast-GEM_8-6-0.xml", growth_id="r_2111", biomass_id="r_4041"
+    )
+    wt.biomass_component_list = biomass_component_list_orig
+    wt.model.reactions.r_1714.bounds = glucose_bounds
+    wt.add_media_components(["r_1992"])
+
+    exch_rate_dict = {
+        "r_1714": np.linspace(0, 2 * 4.75, 8),  # glucose
+        "r_2033": np.linspace(0, 2 * 13.32, 8),  # pyruvate
+        "r_1654": np.linspace(0, 2 * 2.88, 8),  # ammonium
+    }
+
+else:
+    m = plot_options["model"]
+    print(f"Invalid model {m}")
+
+
+if plot_options["carbon_source"] == "glc":
+    exch_rate_dict.pop("r_2033")
+elif plot_options["carbon_source"] == "pyr":
+    exch_rate_dict.pop("r_1714")
+
+ablation_result_array = wt.ablation_grid(exch_rate_dict)
 
 piechart_ablation_grid(
     exch_rate_dict,
