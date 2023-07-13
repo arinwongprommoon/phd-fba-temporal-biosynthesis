d5216a228485fa52da04ed577176cd792894e41f
arinwongprommoon <arin.wongprommoon@ed.ac.uk>
2023-07-13 21:25:35 +0100

    change(dev): axis options

--- dev/dev_streamplot.py
+++ dev/dev_streamplot.py
@@ -5,6 +5,7 @@ import numpy as np
 import pickle
 import seaborn as sns
 
+from matplotlib import transforms
 from src.calc.matrix import get_susceptibility
 
 
@@ -32,12 +33,14 @@ sus = get_susceptibility(growthrate_array, x_axis, y_axis)
 
 breakpoint()
 
-X, Y = np.meshgrid(x_axis, y_axis)
+# X, Y = np.meshgrid(x_axis, y_axis)
+X, Y = np.meshgrid(np.linspace(0, 31, 32), np.linspace(0, 31, 32))
 magnitudes = np.sqrt(sus[0] ** 2, sus[1] ** 2)
 
 breakpoint()
 
+base = plt.gca().transData
+rot = transforms.Affine2D().rotate_deg(270)
+
 plt.streamplot(X, Y, sus[0], sus[1], color=magnitudes, cmap="magma")
 plt.show()
-
-breakpoint()
