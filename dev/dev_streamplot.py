655667eb2fc467929c2a0c1788c75d9e92461a05
arinwongprommoon <arin.wongprommoon@ed.ac.uk>
2023-07-13 21:11:11 +0100

    feat(dev): streamplot

---
+++ dev/dev_streamplot.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pickle
+import seaborn as sns
+
+from src.calc.matrix import get_susceptibility
+
+
+@np.vectorize
+def vget_growthrate(x):
+    return x.ablated_flux[0]
+
+
+filepath = "../data/interim/ec_grid_glc_amm.pkl"
+with open(filepath, "rb") as handle:
+    ablation_result_array = pickle.load(handle)
+
+
+saturation_glc = 8.6869
+saturation_amm = 1.4848
+
+x_axis = np.linspace(0, 2 * saturation_glc, 32)
+y_axis = np.linspace(0, 2 * saturation_amm, 32)
+
+growthrate_array = vget_growthrate(ablation_result_array)
+growthrate_array[0, :] = np.nan
+growthrate_array[:, 0] = np.nan
+
+sus = get_susceptibility(growthrate_array, x_axis, y_axis)
+
+breakpoint()
+
+X, Y = np.meshgrid(x_axis, y_axis)
+magnitudes = np.sqrt(sus[0] ** 2, sus[1] ** 2)
+
+breakpoint()
+
+plt.streamplot(X, Y, sus[0], sus[1], color=magnitudes, cmap="magma")
+plt.show()
+
+breakpoint()
