9d41cc6a94d0d4c97e1f4e350f13d7c0cea91a70
arinwongprommoon <arin.wongprommoon@ed.ac.uk>
2023-07-17 21:08:12 +0100

    refactor: compare_enz_use nb --> script

---
+++ scripts/script_compare_enz_use.py
@@ -0,0 +1,155 @@
+#!/usr/bin/env python3
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+from matplotlib.backends.backend_pdf import PdfPages
+from scipy.spatial.distance import pdist, squareform
+from sklearn.decomposition import PCA
+from src.gem.yeast8model import Yeast8Model
+
+plot_choices = {
+    "euclidean": True,
+    "hierarchical": True,
+    "pca": True,
+    "nonzero": True,
+    "topflux": True,
+    "topflux/ntop": 200,
+}
+
+
+def and_wrapper(a, b):
+    return np.sum(np.logical_and(a, b))
+
+
+def get_topn_list(series, ntop):
+    """Get top N flux-carrying reactions from a Series."""
+    return series.sort_values(ascending=False)[:ntop].index.to_list()
+
+
+def rxns_to_hues(rxn_list, hue_lookup):
+    """Convert reactions to hues"""
+    hues = []
+    for rxn_id in rxn_list:
+        try:
+            hue = hue_lookup[rxn_id]
+            hues.append(hue)
+        except KeyError:
+            hues.append(np.nan)
+    return hues
+
+
+glc_exch_rate = 16.89
+
+# TODO: Add options to exchange glucose/pyruvate and ammonium exchange
+wt = Yeast8Model("../data/gemfiles/ecYeastGEM_batch_8-6-0.xml")
+wt.model.reactions.get_by_id("r_1714").bounds = (-glc_exch_rate, 0)
+wt.model.reactions.get_by_id("r_1714_REV").bounds = (0, glc_exch_rate)
+wt.solution = wt.optimize()
+wt.ablation_result = wt.ablate()
+ablation_fluxes = wt.ablation_fluxes
+
+# Convert dictionary of pandas dataframes to numpy array for various inputs
+enz_use_array = np.stack([df.to_numpy() for df in ablation_fluxes.values()])
+# Remove enzymes that have all-zeros across components
+# because (a) they're not informative,
+# (b) they cause problems in downstream functions
+enz_use_array = enz_use_array[:, np.any(enz_use_array, axis=0)]
+
+list_components = list(ablation_fluxes.keys())
+
+# TODO: Add option to standardise vector (z-scores)
+
+if plot_choices["euclidean"]:
+    distances = pdist(enz_use_array, metric="euclidean")
+    distance_matrix = squareform(distances)
+    distance_triangle = np.tril(distance_matrix)
+    distance_triangle[np.triu_indices(distance_triangle.shape[0])] = np.nan
+
+    fig_euclidean, ax_euclidean = plt.subplots()
+    sns.heatmap(
+        distance_triangle,
+        xticklabels=list_components,
+        yticklabels=list_components,
+        cmap="cividis_r",
+        cbar_kws={"label": "Pairwise Euclidean distances of flux vectors"},
+        ax=ax_euclidean,
+    )
+
+if plot_choices["hierarchical"]:
+    enz_use_df = pd.DataFrame(enz_use_array, index=list_components)
+
+    sns.clustermap(
+        enz_use_df,
+        cbar_kws={"label": "Fluxes"},
+        col_cluster=False,
+        dendrogram_ratio=0.5,
+    )
+
+if plot_choices["pca"]:
+    pca = PCA()
+    Xt = pca.fit_transform(enz_use_array)
+    fig, ax = plt.subplots(figsize=(4.5, 4.5))
+    pca1 = Xt[:, 0]
+    pca2 = Xt[:, 1]
+
+    ax.scatter(pca1, pca2)
+    ax.set_xlabel("PC1")
+    ax.set_ylabel("PC2")
+    for idx, label in enumerate(list_components):
+        ax.annotate(label, (pca1[idx], pca2[idx]))
+
+if plot_choices["nonzero"]:
+    enz_use_nonzero = enz_use_array == 0
+    commons = pdist(enz_use_nonzero, and_wrapper)
+    commons_matrix = squareform(commons)
+    commons_triangle = np.tril(commons_matrix)
+    commons_triangle[np.triu_indices(commons_triangle.shape[0])] = np.nan
+
+    fig, ax = plt.subplots()
+    sns.heatmap(
+        commons_triangle,
+        annot=True,
+        fmt=".0f",
+        xticklabels=list_components,
+        yticklabels=list_components,
+        cmap="cividis",
+        cbar_kws={"label": "Number of enzymes in common with nonzero flux"},
+        ax=ax,
+    )
+
+if plot_choices["topflux"]:
+    ntop = plot_choices["topflux/ntop"]
+
+    # List of top N reactions, original (un-ablated)
+    original_topn_list = get_topn_list(ablation_fluxes["original"], ntop)
+
+    # Assign 'hues' and create lookup table
+    hue_lookup = dict((zip(original_topn_list, range(ntop))))
+
+    # Find hues for all components
+    hues_array = []
+    for series in ablation_fluxes.values():
+        topn_list = get_topn_list(series, ntop)
+        hues = rxns_to_hues(topn_list, hue_lookup)
+        hues_array.append(hues)
+
+    hues_array = np.array(hues_array).T
+
+    # Visualise
+    fig, ax = plt.subplots(figsize=(5, 8))
+    sns.heatmap(
+        hues_array,
+        xticklabels=list_components,
+        cmap="magma_r",
+        cbar=False,
+    )
+    ax.set_xlabel("Biomass component")
+    ax.set_ylabel("Rank")
+
+filename = "compare_enz_use"
+pdf_filename = "../reports/" + filename + ".pdf"
+with PdfPages(pdf_filename) as pdf:
+    for fig in range(1, plt.gcf().number + 1):
+        pdf.savefig(fig)
