7be8c5c1137791dd346775bfb26f171bafe1cd79
Arin Wongprommoon <arin.wongprommoon@ed.ac.uk>
2023-07-20 17:01:57 +0100

    change(scripts/ceu): plot options

--- scripts/script_compare_enz_use.py
+++ scripts/script_compare_enz_use.py
@@ -19,9 +19,9 @@ plot_choices = {
 }
 
 model_options = {
-    "glc_exch_rate": 16.89,
+    "glc_exch_rate": 0.194 * 8.6869,
     "pyr_exch_rate": None,
-    "amm_exch_rate": None,
+    "amm_exch_rate": 0.71 * 1.4848,
 }
 
 compute_options = {
