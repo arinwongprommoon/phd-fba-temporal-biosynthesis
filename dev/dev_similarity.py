254da23ca41e9bef6bd39b4d611932466b7abeb6
Arin Wongprommoon <arin.wongprommoon@ed.ac.uk>
2023-08-01 12:35:02 +0100

    [WIP] refactor(dev): design function & loop to process usgfluxes_grid output
    WHY IS THIS CHANGE NEEDED?:
    - prototyping
    
    HOW DOES THE CHANGE SOLVE THE PROBLEM?:
    - my design pattern:
      - generate array of enzyme usage fluxes using .usgfluxes_grid()
      - save that somewhere
      - load that in
      - have some functions that compute metrics
      - use a double for loop to feed the enzyme usage fluxes into these
        functions, and the output should go to another 2D array
    
    REFERENCES:
    - #62

---
+++ dev/dev_similarity.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import pickle
+
+from scipy.spatial.distance import pdist
+
+
+def cosdist_carb_prot(enz_use_array):
+    distances = pdist(enz_use_array, metric="cosine")
+    metric = distances[7]
+    return metric
+
+
+# Alternatively, load if saved
+with open("../data/interim/ec_usg_glc_amm.pkl", "rb") as handle:
+    ablation_result_array = pickle.load(handle)
+
+breakpoint()
+
+metric_array = np.zeros(shape=ablation_result_array.shape)
+
+for x_index in range(ablation_result_array.shape[0]):
+    for y_index in range(ablation_result_array.shape[1]):
+        metric_array[x_index, y_index] = cosdist_carb_prot(
+            ablation_result_array[x_index, y_index]
+        )
+
+print(metric_array)
+
+breakpoint()
