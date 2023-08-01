1468346e280c4c7f5fecec3791ea8b5f7d28f1b4
Arin Wongprommoon <arin.wongprommoon@ed.ac.uk>
2023-08-01 14:06:52 +0100

    feat(src/calc): mean kendall's tau of original vs all components
    WHY IS THIS CHANGE NEEDED?:
    - Logic: -- "If sequential is better, we expect the flux to change a lot
      from original (i.e. the column headed by 'original' should have blue
      pixels)".  This proves the hypothesis.
    
    REFERENCES:
    - #62
    
    Former-commit-id: 5749c3b4d24c66de515c3bc36c25b1408f6daf05

--- src/calc/ablation.py
+++ src/calc/ablation.py
@@ -196,7 +196,7 @@ def get_kendall_carb_prot(enz_use_array):
         enz_use_array,
         lambda u, v: kendalltau(u, v, nan_policy="omit").statistic,
     )
-    # 7: distance between carbohydrate and protein
+    # 13: distance between carbohydrate and protein
     metric = distances[13]
     return metric
 
@@ -205,6 +205,23 @@ def get_kendall_carb_prot(enz_use_array):
 def vget_kendall_carb_prot(ablation_flux_array):
     """TODO: Insert docstring"""
     return get_kendall_carb_prot(ablation_flux_array)
+
+
+def get_kendall_original(enz_use_array):
+    """TODO: Insert docstring"""
+    distances = pdist(
+        enz_use_array,
+        lambda u, v: kendalltau(u, v, nan_policy="omit").statistic,
+    )
+    # distances[0] to distances[6] are original vs all the other biomass components
+    metric = np.mean(distances[0:7])
+    return metric
+
+
+@np.vectorize
+def vget_kendall_original(ablation_flux_array):
+    """TODO: Insert docstring"""
+    return get_kendall_original(ablation_flux_array)
 
 
 def get_cosine_carb_prot(enz_use_array):
