6f30211d72d6ac9ad9756c78e683d51f4643bcb6
Arin Wongprommoon <arin.wongprommoon@ed.ac.uk>
2023-08-01 12:41:09 +0100

    [WIP] refactor(dev): vectorize

--- dev/dev_similarity.py
+++ dev/dev_similarity.py
@@ -6,6 +6,11 @@ import pickle
 from scipy.spatial.distance import pdist
 
 
+@np.vectorize
+def vcosdist_carb_prot(ablation_result_array):
+    return cosdist_carb_prot(ablation_result_array)
+
+
 def cosdist_carb_prot(enz_use_array):
     distances = pdist(enz_use_array, metric="cosine")
     metric = distances[7]
@@ -25,6 +30,12 @@ for x_index in range(ablation_result_array.shape[0]):
         metric_array[x_index, y_index] = cosdist_carb_prot(
             ablation_result_array[x_index, y_index]
         )
+
+print(metric_array)
+
+breakpoint()
+
+metric_array = vcosdist_carb_prot(ablation_result_array)
 
 print(metric_array)
 
